34737c589214d83869be0ce1f6e7336840b23544
ciki6 <18694942447@163.com>
2019-11-20 10:42:22 +0800

    添加IO文件管理以及Bloc状态管理

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
-import 'package:flutter/material.dart';
+/*
+ * @Author: LuXiaoFu 
+ * @Date: 2019-11-20 10:36:22 
+ * @Last Modified by: LuXiaoFu
+ * @Last Modified time: 2019-11-20 10:41:06
+ */
 
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc.dart';
 import 'dy_index.dart';
 import 'dy_room.dart';
 
-class DyApp extends StatefulWidget {
-  @override
-  DyAppState createState() => DyAppState();
-}
-class DyAppState extends State<DyApp> {
+class DyApp extends StatelessWidget {
+  //动态路由传递参数
   Route<dynamic> _getRoute(RouteSettings settings) {
     Map<String, WidgetBuilder> routes = {
-      '/':     (BuildContext context) => DyIndexPage(),
-      '/room': (BuildContext context) => DyRoomPage(arguments: settings.arguments),
+      '/': (BuildContext context) => DyIndexPage(),
+      '/room': (BuildContext context) =>
+          DyRoomPage(arguments: settings.arguments),
     };
     var route = routes[settings.name];
 
@@ -27,13 +34,16 @@ class DyAppState extends State<DyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'DYFlutter',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Color.fromARGB(255, 255, 255, 255),
-        primarySwatch: Colors.orange,
+    return BlocProvider<CounterBloc>(
+      builder: (context) => CounterBloc(),
+      child: MaterialApp(
+        title: 'DYFlutter',
+        theme: ThemeData(
+          scaffoldBackgroundColor: Color.fromARGB(255, 255, 255, 255),
+          primarySwatch: Colors.orange,
+        ),
+        onGenerateRoute: _getRoute,
       ),
-      onGenerateRoute: _getRoute,
     );
   }
 }
